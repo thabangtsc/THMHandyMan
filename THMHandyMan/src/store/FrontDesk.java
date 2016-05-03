d2567296ce3cc6a78f1fc4e433666a4f79b89277
thabangtsc <thabangmoepi@gmail.com>
2016-05-03 15:54:48 +0200

    Remove Welcome message

--- THMHandyMan/src/store/FrontDesk.java
+++ THMHandyMan/src/store/FrontDesk.java
@@ -1,24 +1,27 @@
-package store;
+package store; 
+ 
+ 
+ import javax.swing.JOptionPane; 
+ /** 
+  * Main Class 
+  * @author Thabang Moepi 
+  * 
+  */ 
+ public class FrontDesk { 
 
-import javax.swing.JOptionPane;
-/**
- * Main Class
- * @author Thabang Moepi
- *
- */
-public class FrontDesk {
-
-	public static void main(String[] args) 
-	{
-		
-		JOptionPane.showMessageDialog(null, "Welcome to THM Handyman Store");
-		System.out.println("The following are records of the database");
-		/*
-		 * Call methods from class TheStore
-		 */
-		
-		TheStore.SelectVendor();
-		TheStore.SelectVendorProduct();
-	}
-
-}
+ 
+ 	public static void main(String[] args)  
+ 	{ 
+ 		 
+		 
+ 		System.out.println("The following are records of the database"); 
+ 		/* 
+		 * Call methods from class TheStore 
+		 */ 
+ 		 
+ 		TheStore.SelectVendor(); 
+ 		TheStore.SelectVendorProduct(); 
+ 	} 
+ 
+ 
+ } 
